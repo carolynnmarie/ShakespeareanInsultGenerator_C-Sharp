cc24a7b56bf46a09c84e510cb4737f2c5276b991
Carolynn Vansant <carolynnmarie@gmail.com>
2018-08-22 15:35:25 -0400

    changed project structure for better encapsulation

--- Program.cs
+++ Program.cs
@@ -7,10 +7,15 @@ namespace C_Sharp_Shakespeare
         static void Main(string[] args)
         {
             RandomInsultGenerator generator = new RandomInsultGenerator();
+            string repeat = "";
+            do{
             Console.WriteLine("If you would like to create your own random Shakespearean insult, type random. If you would" +
             " like a classic Shakespearean insult, type classic");
             string answer = Console.ReadLine();
-            Console.WriteLine(generator.pickRandomOrClassic(answer));
+            Console.WriteLine("\n" + generator.pickRandomOrClassic(answer));
+            Console.WriteLine("\nWould you like to go again?  Type y for yes, or any other key to exit.");
+            repeat = Console.ReadLine();
+            } while(repeat == "y");
         }
     }
 }
